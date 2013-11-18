7c23b65bfb1fc710ee1d007cd9fdac17de87a951
phoenixbox <shane@quickleft.com>
2013-11-18 12:04:07 -0700

    inclusion and import of mapkit and the declaration of the necessary instance varaibles in the header file

--- Whereami/WhereamiViewController.h
+++ Whereami/WhereamiViewController.h
@@ -8,11 +8,16 @@
 
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
 
 @interface WhereamiViewController : UIViewController <CLLocationManagerDelegate>
 
 {
     CLLocationManager *locationManager;
+    
+    IBOutlet MKMapView *worldView;
+    IBOutlet UIActivityIndicatorView *activityIndicator;
+    IBOutlet UITextField *locationTitleField;
 }
 
 @end
